7b1250676403f92d8b165f8e07e928450238f57e
Savelyeva_A <Aliaksandra.Savelyeva@ibm.com>
2021-07-28 19:50:23 +0300

    hash passwords

--- src/main/java/by/alia/servletproject/GroupListServlet.java
+++ src/main/java/by/alia/servletproject/GroupListServlet.java
@@ -1,5 +1,6 @@
 package by.alia.servletproject;
 
+import by.alia.servletproject.dao.PersonDao;
 import by.alia.servletproject.model.ListService;
 import by.alia.servletproject.model.Person;
 
@@ -11,26 +12,28 @@ import java.io.IOException;
 @WebServlet(name = "GroupListServlet", value = "/GroupListServlet")
 public class GroupListServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
-    private ListService todoService = new ListService();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("group", ListService.retrieveList());
+        PersonDao daoPerson = new PersonDao();
+        request.setAttribute("group",daoPerson.getPersons());
         request.getRequestDispatcher("/WEB-INF/views/welcome.jsp")
                 .forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PersonDao daoPerson = new PersonDao();
         String nname = request.getParameter("nname");
         String nphone = request.getParameter("nphone");
         String nemail= request.getParameter("nemail");
-        if (("".equals(nname))|| ("".equals(nphone)) || ("".equals(nemail))) {
+        if (nname.isBlank() || nphone.isBlank() || nemail.isBlank()) {
             request.setAttribute("errorMessage", "Заполните все поля");
         } else {
-            ListService.addPerson(new Person(nname, nphone,nemail));
+            daoPerson.insertPerson(new Person(nname, nphone,nemail));
         }
-        request.setAttribute("group", ListService.retrieveList());
+
+        request.setAttribute("group", daoPerson.getPersons());
         request.getRequestDispatcher("/WEB-INF/views/welcome.jsp")
                 .forward(request, response);
     }
